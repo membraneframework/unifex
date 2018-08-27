823d49d5831bd0d1d07e04b5e023138f1dac6679
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-08-27 16:37:02 +0200

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Unifex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @github_link "https://github.com/membraneframework/unifex"
+
   def project do
     [
       app: :unifex,
       compilers: [:bundlex] ++ Mix.compilers(),
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      name: "Unifex",
+      description: "An abstraction over native code",
+      source_url: @github_link,
+      package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -15,6 +23,26 @@ defmodule Unifex.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Membrane Team"],
+      licenses: ["Apache 2.0"],
+      files: ["lib", "c_src", "mix.exs", "README*", "LICENSE*", ".formatter.exs", "bundlex.exs"],
+      links: %{
+        "GitHub" => @github_link,
+        "Membrane Framework Homepage" => "https://membraneframework.org"
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"],
+      source_ref: "v#{@version}"
     ]
   end
 
