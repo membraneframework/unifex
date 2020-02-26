0b96316781baa64b27a320e82143d9e8a1fc24cb
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2020-02-26 11:59:25 +0100

    refactoring

--- lib/unifex/code_generation_mode.ex
+++ lib/unifex/code_generation_mode.ex
@@ -7,33 +7,14 @@ defmodule Unifex.CodeGenerationMode do
         }
 
   @spec code_generation_mode(
-          name :: String.t(),
-          dir :: any,
-          _specs :: Unifex.SpecsParser.parsed_specs_t()
+          _name :: String.t(),
+          _dir :: any,
+          specs :: Unifex.SpecsParser.parsed_specs_t()
         ) :: t()
-  def code_generation_mode(name, dir, specs) do
+  def code_generation_mode(_name, _dir, specs) do
     %__MODULE__{
       use_state: specs |> Keyword.get(:use_state, false),
       cnode_mode: specs |> Keyword.get(:cnode_mode, false)
     }
-  end
-
-  defp state_definition_exists(dir, name) do
-    state_type = "UnifexState"
-    contains_word(dir, name, state_type)
-  end
-
-  defp old_state_definition_exists(dir, name) do
-    old_state_type = "UnifexNifState"
-    contains_word(dir, name, old_state_type)
-  end
-
-  defp contains_word(dir, header_name, word) do
-    header_path = Path.join(dir, header_name <> ".h")
-
-    File.stream!(header_path)
-    |> Enum.any?(fn
-      line -> line |> String.contains?(word)
-    end)
   end
 end
