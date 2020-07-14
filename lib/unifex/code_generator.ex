68c99a19d2680d689e2fb014b1590a48fa5d607d
mickel8 <michal.sledz@swmansion.com>
2020-07-14 15:18:09 +0200

    Add support for :natives key in bundlex

--- lib/unifex/code_generator.ex
+++ lib/unifex/code_generator.ex
@@ -23,15 +23,36 @@ defmodule Unifex.CodeGenerator do
   defp get_generator(%Specs{name: name, interface: nil}) do
     {:ok, bundlex_project} = Bundlex.Project.get()
 
-    [:nifs, :cnodes]
+    [:nifs, :cnodes, :natives, :libs]
     |> Enum.find(&(bundlex_project.config |> Keyword.get(&1, []) |> Keyword.has_key?(name)))
     |> case do
-      :nifs -> Unifex.CodeGenerators.NIF
-      :cnodes -> Unifex.CodeGenerators.CNode
+      :nifs ->
+        Unifex.CodeGenerators.NIF
+
+      :cnodes ->
+        Unifex.CodeGenerators.CNode
+
+      :natives ->
+        interfaces = bundlex_project.config[:natives][name][:interfaces]
+        get_generator(get_generator_module_name(List.first(interfaces)))
+
+      _ ->
+        Unifex.CodeGenerators.NIF
     end
   end
 
   defp get_generator(%Specs{interface: interface}) do
+    get_generator(interface)
+  end
+
+  defp get_generator(interface) do
     Module.concat(Unifex.CodeGenerators, interface)
+  end
+
+  defp get_generator_module_name(interface) do
+    case interface do
+      :nif -> :NIF
+      :cnode -> :CNode
+    end
   end
 end
