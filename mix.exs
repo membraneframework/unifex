213c1b9a1c18c64b8f4b2b50e647c96afe34c30e
mickel8 <michal.sledz@swmansion.com>
2020-07-20 10:05:30 +0200

    Set temporary bundlex version

--- mix.exs
+++ mix.exs
@@ -60,7 +60,10 @@ defmodule Unifex.MixProject do
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:bunch, "~> 1.0"},
       {:shmex, "~> 0.2.0"},
-      {:bundlex, "~> 0.2.0"},
+      {:bundlex,
+       git: "https://github.com/membraneframework/bundlex.git",
+       branch: "natives-spec",
+       override: true},
       {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
     ]
   end
