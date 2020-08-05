e5b3a887783d20c8dc8b2dd892a7b99761124899
Michał Śledź <michal.sledz@swmansion.com>
2020-08-05 15:15:22 +0200

    Implement unsigned int for CNodes

--- lib/unifex/code_generator/base_types/unsigned.ex
+++ lib/unifex/code_generator/base_types/unsigned.ex
@@ -25,4 +25,32 @@ defmodule Unifex.CodeGenerator.BaseTypes.Unsigned do
       ~g<enif_get_uint(env, #{arg_term}, &#{var_name})>
     end
   end
+
+  defmodule CNode do
+    @moduledoc false
+    use Unifex.CodeGenerator.BaseType
+    alias Unifex.CodeGenerator.BaseType
+
+    @impl BaseType
+    def generate_arg_serialize(name, _ctx) do
+      ~g"""
+      ({
+        unsigned int #{name}_uint = #{name};
+        ei_x_encode_ulonglong(out_buff, (unsigned long long)#{name}_uint);
+      });
+      """
+    end
+
+    @impl BaseType
+    def generate_arg_parse(argument, name, _ctx) do
+      ~g"""
+      ({
+        unsigned long long #{name}_ulonglong;
+        int result = ei_decode_ulonglong(#{argument}->buff, #{argument}->index, &#{name}_ulonglong);
+        #{name} = (unsigned int)#{name}_ulonglong;
+        result;
+      })
+      """
+    end
+  end
 end
