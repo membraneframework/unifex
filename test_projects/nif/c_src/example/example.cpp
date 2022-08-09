6fb16fbd3d70c660f901cb45f08bf2b99a64bacc
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-08-09 17:19:24 +0200

    Fix warns caused by compiling *.c files by C++ compiler

---
+++ test_projects/nif/c_src/example/example.cpp
@@ -0,0 +1,81 @@
+#include "example.h"
+
+int example_was_handle_load_called = 0;
+
+int handle_load(UnifexEnv *env, void **priv_data) {
+  UNIFEX_UNUSED(env);
+  UNIFEX_UNUSED(priv_data);
+  example_was_handle_load_called = 1;
+  return 0;
+}
+
+UNIFEX_TERM init(UnifexEnv *env) {
+  MyState *state = unifex_alloc_state(env);
+  state->a = 42;
+  UNIFEX_TERM res = init_result_ok(env, example_was_handle_load_called, state);
+  unifex_release_state(env, state);
+  return res;
+}
+
+UNIFEX_TERM test_atom(UnifexEnv *env, char *in_atom) {
+  return test_atom_result_ok(env, in_atom);
+}
+
+UNIFEX_TERM test_float(UnifexEnv *env, double in_float) {
+  return test_float_result_ok(env, in_float);
+}
+
+UNIFEX_TERM test_int(UnifexEnv *env, int in_int) {
+  return test_int_result_ok(env, in_int);
+}
+
+UNIFEX_TERM test_string(UnifexEnv *env, char *str) {
+  return test_string_result_ok(env, str);
+}
+
+UNIFEX_TERM test_list(UnifexEnv *env, int *in_list, unsigned int list_length) {
+  return test_list_result_ok(env, in_list, list_length);
+}
+
+UNIFEX_TERM test_list_of_strings(UnifexEnv *env, char **in_strings,
+                                 unsigned int list_length) {
+  return test_list_of_strings_result_ok(env, in_strings, list_length);
+}
+
+UNIFEX_TERM test_pid(UnifexEnv *env, UnifexPid in_pid) {
+  return test_pid_result_ok(env, in_pid);
+}
+
+UNIFEX_TERM test_state(UnifexEnv *env, MyState *state) {
+  return test_state_result_ok(env, state);
+}
+
+UNIFEX_TERM test_example_message(UnifexEnv *env, UnifexPid pid) {
+  int res = send_example_msg(env, pid, 0, 10);
+  if (!res) {
+    return test_example_message_result_error(env, "send_failed");
+  }
+  return test_example_message_result_ok(env);
+}
+
+UNIFEX_TERM test_my_struct(UnifexEnv *env, my_struct in_struct) {
+  return test_my_struct_result_ok(env, in_struct);
+}
+
+UNIFEX_TERM test_my_enum(UnifexEnv *env, MyEnum in_enum) {
+  return test_my_enum_result_ok(env, in_enum);
+}
+
+UNIFEX_TERM test_nested_struct(UnifexEnv *env, nested_struct in_struct) {
+  return test_nested_struct_result_ok(env, in_struct);
+}
+
+UNIFEX_TERM test_list_of_structs(UnifexEnv *env, simple_struct *structs,
+                                 unsigned int structs_length) {
+  return test_list_of_structs_result_ok(env, structs, structs_length);
+}
+
+void handle_destroy_state(UnifexEnv *env, MyState *state) {
+  UNIFEX_UNUSED(env);
+  state->a = 0;
+}
---
+++ test_projects/unified/c_src/example/example.cpp
@@ -0,0 +1,3 @@
+#include "example.h"
+
+UNIFEX_TERM foo(UnifexEnv *env, int num) { return foo_result_ok(env, num); }
