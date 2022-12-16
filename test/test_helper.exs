a81cc99c273eb43d815d4e15e0060316e897f9ee
Michał Śledź <michalsledz34@gmail.com>
2022-12-16 16:17:59 +0100

    Format generated code only when clang-format is available (#101)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,7 @@
-ExUnit.start()
+# clang-format is required to format the generated code
+# it won't match the reference files otherwise
+if Unifex.Utils.clang_format_installed?() do
+  ExUnit.start()
+else
+  raise "clang-format has to be installed on the system to run Unifex tests."
+end
