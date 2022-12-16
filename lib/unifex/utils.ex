d2cb34bf67cb4e556d935fca36138f06ec1ff429
Michał Śledź <michalsledz34@gmail.com>
2022-12-16 16:04:17 +0100

    Format code, bump version

--- lib/unifex/utils.ex
+++ lib/unifex/utils.ex
@@ -2,7 +2,5 @@ defmodule Unifex.Utils do
   @moduledoc false
 
   @spec clang_format_installed?() :: boolean()
-  def clang_format_installed?() do
-    if System.find_executable("clang-format"), do: true, else: false
-  end
+  def clang_format_installed?(), do: System.find_executable("clang-format") != nil
 end
